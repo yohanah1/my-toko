f0a2a230d5222fc120cf2c714a241579d823b200
rodericusifo <rodericus1999@gmail.com>
2021-05-22 22:10:25 +0700

    add page loader component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { PageLoaderComponent } from './components/page-loader/page-loader.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PageLoaderComponent],
+  imports: [CommonModule],
+  exports: [PageLoaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
