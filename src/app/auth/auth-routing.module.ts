ed13a811239e1963b374ecc2ad9d2bbb50714795
Rizka Indah Puspita <rizkaindahpuspita@gmail.com>
2021-05-27 16:45:37 +0700

    add forget password and home

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,14 @@
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PasswordRequestComponent } from './password-request/password-request.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'password/reset-request', component: PasswordRequestComponent },
+  { path: 'password/reset', component: PasswordResetComponent },
 ];
 
 @NgModule({
